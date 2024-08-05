3adae93a0774c232eb4acd9dccabe4f465ca6c65
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-04 23:38:13 -0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <h1 className={styles.main}>test style</h1>
     </main>
   );
