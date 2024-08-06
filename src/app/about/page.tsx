31536ead501ef9dc27de9a8d7f2ffaa7d721c173
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-05 23:33:47 -0300

    feat: test

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,11 +2,11 @@ import styles from "../page.module.css";
 
 export default function About() {
   return (
-    <main>
+    <main className={styles.main}>
       <div className="container">
         <h2>test page</h2>
       </div>
-      <h1 className={styles.main}>test about</h1>
+      <h1>test about</h1>
     </main>
   );
 }
