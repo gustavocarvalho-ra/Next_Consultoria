e7a03cdabbdb676234009d087af287d9b1d931bc
GustavoCR021 <gustavoc1922@gmail.com>
2024-08-05 23:20:45 -0300

    feat: test page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+
+export default function About() {
+  return (
+    <main>
+      <div className="container">
+        <h2>test page</h2>
+      </div>
+      <h1 className={styles.main}>test style</h1>
+    </main>
+  );
+}
